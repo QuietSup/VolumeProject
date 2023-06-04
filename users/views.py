fa8ea99ac4df0b29f19f72ab0cff2062582a8819
QuietSup <feeling_of_infinity@outlook.com>
2023-06-04 20:20:32 +0300

    Show user profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout
 from users.forms import SignupForm, LoginForm, UpdateUserForm
 from django.contrib.auth.decorators import login_required
+from Volume.models import User
+from django.utils.text import slugify
 
 
 # Create your views here.
@@ -48,3 +50,12 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('users:login')
+
+def profile(request, slug, user_id):
+
+    user = get_object_or_404(User, pk=user_id)
+
+    if slugify(user.username) != slug:
+        return redirect('users:profile', slug=slugify(user.username), user_id=user_id)
+
+    return render(request, 'users/profile.html', {'user': user})
