db65eef23dbbdfe2ab68a84124c38d88812eecb3
QuietSup <feeling_of_infinity@outlook.com>
2023-06-07 19:04:23 +0300

    Changed home page to explore page

--- VolumeProject/urls.py
+++ VolumeProject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import pins.views
 from django.contrib import admin
 from django.urls import path, include
 from Volume import views
@@ -22,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', pins.views.explore()),
     path('users/', include('users.urls')),
     path('pins/', include('pins.urls')),
 ]
