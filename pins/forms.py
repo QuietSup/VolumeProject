4e7f3a9531cbad51a823a59b181995a68acc865e
QuietSup <feeling_of_infinity@outlook.com>
2023-06-07 09:56:30 +0300

    Added "edit pin" page

--- pins/forms.py
+++ pins/forms.py
@@ -37,3 +37,47 @@ class CreatePinForm(forms.ModelForm):
     class Meta:
         model = Pin
         fields = ['image', 'title', 'description']
+
+
+class UpdatePinForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['image'] = forms.ImageField(
+            label='Image',
+            required=False,
+            widget=forms.FileInput(
+                attrs={
+                    'id': 'px_file',
+                    'class': 'px-input-file',
+                }
+            )
+        )
+
+        self.fields['title'] = forms.CharField(
+            label='Title',
+            initial=self.instance.title,
+            widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'e. g. Marioni with logo',
+                }
+            )
+        )
+
+        self.fields['description'] = forms.CharField(
+            label='Description',
+            initial=self.instance.description,
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Your legendary description',
+                    'rows': '5'
+                }
+            )
+        )
+
+    class Meta:
+        model = Pin
+        fields = ['image', 'title', 'description']
+
