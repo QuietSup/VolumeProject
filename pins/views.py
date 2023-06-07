db3aa1d8b07dc641b8def9e07d904c5c5b4bf7e5
QuietSup <feeling_of_infinity@outlook.com>
2023-06-07 19:00:20 +0300

    Added search function and fixed styles

--- pins/views.py
+++ pins/views.py
@@ -4,33 +4,61 @@ from Volume.models import Pin
 from pins.forms import CreatePinForm, UpdatePinForm
 from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
+import datetime
+from django.core.paginator import Paginator
 
 
 def explore(request):
-    if request.method == 'POST':
-        pass
-    else:
-        if request.GET.get('sort'):
-            if request.GET.get('sort') == 'justAdded':
-                #  TODO: можна додати прикол типу шо тіки ті шо сьодні додались чи шось таке, якщо хо.
-                pins = Pin.objects.order_by('updated_at')
-            elif request.GET.get('sort') == 'lowToHigh':
-                pins = Pin.objects.order_by('price')
-            elif request.GET.get('sort') == 'highToLow':
-                pins = Pin.objects.order_by('-price')
-            elif request.GET.get('sort') == 'mostLiked':
-                pins = Pin.objects.order_by('-likes')
-            elif request.GET.get('sort') == 'leastLike':
-                pins = Pin.objects.order_by('likes')
-            else:
-                pins = Pin.objects.order_by('updated_at')
-        elif request.GET.get('search'):
-            search = request.GET.get('search')
-            pins = Pin.objects.filter(title__icontains=search) | Pin.objects.filter(description__icontains=search)
-        else:
-            pins = Pin.objects.order_by('updated_at')
+    sort_by = {
+        'Most_liked': '-likes',
+        'Least liked': 'likes',
+        'Latest added': '-created_at',
+        'Earliest added': 'created_at',
+        'Latest updated': '-updated_at',
+        'Earliest updated': 'updated_at',
+    }
 
-    return render(request, 'pins/explore.html', {'pins': pins, 'current_user': request.user})
+    when_created = {
+        'last_6hours': 'Last six hours',
+        'last_day': 'Last day',
+        'last_2days': 'Last two days',
+        'last_week': 'Last week',
+        'last_month': 'Last month',
+        'last_year': 'Last year',
+    }
+
+    if request.GET.get('sort') and request.GET.get('sort') in sort_by.values():
+        pins = Pin.objects.order_by(request.GET['sort'])
+    else:
+        pins = Pin.objects.order_by('created_at')
+
+    if request.GET.get('when_created') and request.GET.get('when_created') in when_created.keys():
+        if request.GET.get('when_created') == 'last_6hours':
+            delta = datetime.timedelta(hours=6)
+        if request.GET.get('when_created') == 'last_day':
+            delta = datetime.timedelta(days=1)
+        if request.GET.get('when_created') == 'last_2days':
+            delta = datetime.timedelta(days=2)
+        if request.GET.get('when_created') == 'last_week':
+            delta = datetime.timedelta(weeks=1)
+        if request.GET.get('when_created') == 'last_month':
+            delta = datetime.timedelta(weeks=4)
+        if request.GET.get('when_created') == 'last_year':
+            delta = datetime.timedelta(days=365)
+
+        date_from = datetime.datetime.now() - delta
+        pins.filter(created_at__gt=date_from)
+
+    if request.GET.get('search'):
+        pins = Pin.objects.filter(title__icontains=request.GET.get('search')) \
+               | Pin.objects.filter(description__icontains=request.GET.get('search')) \
+               | Pin.objects.filter(creator__username__icontains=request.GET.get('search'))
+
+    paginator = Paginator(pins, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'pins/explore.html', {'current_user': request.user, 'sort_by': sort_by, 'when_created': when_created, 'page_obj': page_obj})
 
 @login_required
 def add_like(request, pinId):
@@ -67,6 +95,7 @@ def show_pin(request, slug, pin_id):
 
     creator_pins = pin.creator.pin_set.order_by('created_at').exclude(pk=pin_id)[:6]
 
+
     return render(request, 'pins/show-pin.html', {'pin': pin, 'creator_pins': creator_pins})
 
 @login_required
@@ -90,7 +119,7 @@ def edit_pin(request, pin_id):
     else:
         form = UpdatePinForm(instance=pin)
 
-    return render(request, 'pins/edit-pin.html', {'form': form, 'pin_id': pin_id})
+    return render(request, 'pins/edit-pin.html', {'form': form, 'pin': pin})
 
 @login_required
 def delete_pin(request, pin_id):
